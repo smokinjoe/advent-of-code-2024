1511f5d629966f883b15fb530c012af05d4350a5
Joe <joe.ekiert@gmail.com>
2024-12-04 21:30:44 -0500

    removed commented code

--- src/days/day04.ts
+++ src/days/day04.ts
@@ -3,48 +3,11 @@ import { readFile, getFilename } from "../util/readFile";
 
 const Day = "04";
 
-// const checkIfValidNorth = (y: number, min: number) => y - 3 >= min;
-// const checkIfValidSouth = (y: number, max: number) => y + 3 <= max;
-// const checkIfValidWest = (x: number, min: number) => x - 3 >= min;
-// const checkIfValidEast = (x: number, max: number) => x + 3 <= max;
-
-// const checkIfValidNorthWest = (x: number, y: number, min: number) =>
-//   checkIfValidNorth(y, min) && checkIfValidWest(x, min);
-// const checkIfValidNorthEast = (
-//   x: number,
-//   y: number,
-//   min: number,
-//   max: number
-// ) => checkIfValidNorth(y, min) && checkIfValidEast(x, max);
-// const checkIfValidSouthWest = (
-//   x: number,
-//   y: number,
-//   min: number,
-//   max: number
-// ) => checkIfValidSouth(y, max) && checkIfValidWest(x, min);
-// const checkIfValidSouthEast = (x: number, y: number, max: number) =>
-//   checkIfValidSouth(y, max) && checkIfValidEast(x, max);
-
 export const partOne = () => {
-  //   const filename = "day-04-mini-example.txt";
   const filename = getFilename(Day);
   const data = readFile(filename);
 
   const xmasCells = data.split("\n").map((row) => row.split(""));
-
-  console.table(xmasCells);
-
-  //   const checkCharacterRecursion = (
-  //     x: number,
-  //     y: number,
-  //     characterArray: string[]
-  //   ) => {
-  //     if (xmasCells[y][x] === undefined) return false;
-  //     if (characterArray.length === 1) {
-  //       // we're checking for final character
-  //       return xmasCells[y][x] === characterArray[0];
-  //     }
-  //   };
 
   const isCorrectCharacter = (
     x: number,
